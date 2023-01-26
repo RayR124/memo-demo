d5833eb7c67f4be036cd0ed04f31565845947b67
RayR124 <RealtorRayR@Gmail.com>
2023-01-26 14:21:50 -0800

    added require path and app.use to see if it doesn anything

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const fs = require("fs");
 const notes = require("./db/db.json");
 const path = require("path");
-const path = require('path');
 const api = require("./routes/api");
 const html = require("./routes/html");
 //const util =  require("util");
