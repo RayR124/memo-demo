2418aab35745d8e76713262726c5b8c3c7244f2a
RayR124 <RealtorRayR@Gmail.com>
2023-01-26 14:21:10 -0800

    many small changes, added path, deleted api/notes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const fs = require("fs");
 const notes = require("./db/db.json");
-//const api = require("./api/notes");
 const path = require("path");
-//const uuid = require("uuid");
+const path = require('path');
+const api = require("./routes/api");
+const html = require("./routes/html");
+//const util =  require("util");
 
 const app = express();
 const port = process.env.port || 3001;
@@ -11,6 +13,8 @@ const port = process.env.port || 3001;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(api);
+app.use(html);
 //app.use("/api", api);
 
 require('./routes/api')(app);
@@ -36,7 +40,7 @@ app.post("./api/notes", (req, res) => {
     res.json(note);
 });
 
-// this should remove the note with the given id property, and then rewrite the notes to the db.json file
+// [bonus] this should remove the note with the given id property, and then rewrite the notes to the db.json file
 app.delete("./api/notes/:id", (req, res) => {
     const note = JSON.parse(fs.readFileSync("./db/db.json"));
     const deleteNote = note.filter((rmvNote) => rmvNote.id !== req.params.id);
