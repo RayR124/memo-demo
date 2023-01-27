db294b9b633016b6579ab72f4881ddc0d10e99df
RayR124 <RealtorRayR@Gmail.com>
2023-01-27 13:16:55 -0800

    updated port to 3306

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.port || 3306;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
